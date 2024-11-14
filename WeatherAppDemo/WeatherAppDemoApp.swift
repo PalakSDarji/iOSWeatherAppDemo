2ec99c904d967c8b32c5d1a8aa8948906bce89c1
Palakkumar Darji <palaksdarji@gmail.com>
2024-11-14 21:08:52 +0530

    Initial Commit

---
+++ WeatherAppDemo/WeatherAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppDemoApp.swift
+//  WeatherAppDemo
+//
+//  Created by Palakkumar Darji on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
